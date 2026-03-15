bfc9e0eff1d1c3429dca53f3e0e8ae2f4f6791b8
Hadeel Ahmed <hadilelarabi@gmail.com>
2026-03-15 00:04:41 -0400

    Add profile page with Firebase auth, patient info, appointments and logout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import BookingForm from './components/pages/BookingForm';
-import './App.css';
-
 // I added a folder for pages and a folder for features. The pages folder will contain the main components that represent each page of the booking process, while the features folder will contain smaller, reusable components that are used within those pages. This structure helps to keep the code organized and makes it easier to manage as the application grows.
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import BookingForm from './components/pages/BookingForm';
+import ProfilePage from './pages/ProfilePage';
+import './App.css';
 export default function App() {
   return (
-    <BookingForm />
-
-  )
+    <Router>
+      <Routes>
+        {/* Default route redirects to /booking */}
+        <Route path="/" element={<Navigate to="/booking" replace />} />
+        <Route path="/booking" element={<BookingForm />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
+  );
 }
