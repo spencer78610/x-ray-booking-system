e2c3c4ac7baa3140c5393d0b9f099199f0fe091e
Hadeel Ahmed <hadilelarabi@gmail.com>
2026-04-02 13:45:14 -0400

    Fix the input spaces in the login

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -17,7 +17,7 @@ const styles = `
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    padding: 48px;
+    padding: 18px;
     position: relative;
     overflow: hidden;
   }
